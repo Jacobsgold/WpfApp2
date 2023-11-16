2a8c671f61c736b7247cdbc8af0404592cea24d1
User <User@NPK107-8>
2023-11-16 15:53:21 +0700

    Добавьте файлы проекта.

---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows;
+
+namespace WpfApp2
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void HamburgerButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Добавьте логику для обработки нажатия кнопки гамбургера
+            // Например, открывайте боковое меню
+        }
+    }
+}
